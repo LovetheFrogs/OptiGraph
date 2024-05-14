b39f09af1103bff961a074959c0b1ff0cbf74a6d
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2024-05-14 17:48:30 +0200

    Renamed Application class

--- OptiGraph/src/main/java/org/lovethefrogs/optigraph/OptiGraphApplication.java
+++ OptiGraph/src/main/java/org/lovethefrogs/optigraph/OptiGraphApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class OptiGraphApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(OptiGraphApplication.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("OptiGraph V0.1");
         stage.setScene(scene);
