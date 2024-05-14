4ab4cb0df57f447ab174c750a0adef7c369673d5
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2024-05-14 17:44:23 +0200

    Feature: Added graph logic

--- OptiGraph/src/main/java/org/lovethefrogs/optigraph/model/Node.java
+++ OptiGraph/src/main/java/org/lovethefrogs/optigraph/model/Node.java
@@ -1,4 +1,58 @@
 package org.lovethefrogs.optigraph.model;
 
+import org.lovethefrogs.optigraph.utils.Coords;
+
+import java.util.ArrayList;
+
 public class Node {
+    private final int id;
+    private String name;
+    private Coords coords;
+    private boolean center;
+
+    private ArrayList<Double> dist;
+
+    public Node(int id, String name, Coords coords, boolean center) {
+        this.id = id;
+        this.name = name;
+        this.coords = coords;
+        this.center = center;
+        this.dist = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Coords getCoords() {
+        return coords;
+    }
+
+    public void setCoords(Coords coords) {
+        this.coords = coords;
+    }
+
+    public boolean isCenter() {
+        return center;
+    }
+
+    public void setCenter(boolean center) {
+        this.center = center;
+    }
+
+    public ArrayList<Double> getDist() {
+        return dist;
+    }
+
+    public void setDist(ArrayList<Double> dist) {
+        this.dist = dist;
+    }
 }
