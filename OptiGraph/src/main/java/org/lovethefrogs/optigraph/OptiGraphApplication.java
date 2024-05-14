056c073ed1af78177e3128402ef9fcfb5faefd89
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2024-05-14 20:18:19 +0200

    Feature: Changed window size

--- OptiGraph/src/main/java/org/lovethefrogs/optigraph/OptiGraphApplication.java
+++ OptiGraph/src/main/java/org/lovethefrogs/optigraph/OptiGraphApplication.java
@@ -11,7 +11,7 @@ public class OptiGraphApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(OptiGraphApplication.class.getResource("home-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("OptiGraph V0.1");
         stage.setScene(scene);
         stage.show();
